7b136e5a96a9818996fc2d21dfa31ab6243ec513
Nalinee04 <162853604+Nalinee04@users.noreply.github.com>
2025-03-09 22:06:35 +0700

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'my-test',
-  webDir: 'public'
+  webDir: 'public',
+  server: {
+    url: ' https://3e69-2001-fb1-18b-9c71-51cf-86df-791d-8bad.ngrok-free.app',
+    cleartext: true
+  }
 };
-
 export default config;
